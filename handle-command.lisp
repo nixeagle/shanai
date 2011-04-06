8fdd80c2f8df4adb93b760b47e3c7ca3659fc808
James S <i@nixeagle.org>
2011-04-06 15:29:20 -0400

    redo ',statranges' and add ',scripts'

--- handle-command.lisp
+++ handle-command.lisp
@@ -137,3 +137,91 @@ Format is #foobar"
                       ,(read-from-string string nil "(:b \"Sorry malformed input. Did you forget a closing paren?\")"))))
     (error (condition) (princ-to-string condition))))
 
+(defun match-level-and-base-stat (command-string)
+  (cl-ppcre:register-groups-bind (a b) ("(\\d+)\\s(\\d+)" command-string)
+    (let ((level (parse-integer a))
+          (base-stat (parse-integer b)))
+      (values level base-stat))))
+
+(defun other-stat-formula (level base iv ev nature)
+  (pokemon::_*_ (+ (pokemon::_*_ (+ iv (* 2 base) (pokemon::_/_ ev 4))
+               level 1/100) 5) (/ nature 100)))
+
+
+(defun other-formula-intervals (level base)
+  (flet ((formula (iv ev nature)
+           (other-stat-formula level base iv ev nature)))
+    (let ((result (make-array '(2 2 3) )))
+      (setf (aref result 0 0 0) (formula 0 0 90)
+            (aref result 0 0 1) (formula 0 0 100)
+            (aref result 0 0 2) (formula 0 0 110)
+            (aref result 0 1 0) (formula 0 252 90)
+            (aref result 0 1 1) (formula 0 252 100)
+            (aref result 0 1 2) (formula 0 252 110)
+            (aref result 1 0 0) (formula 31 0 90)
+            (aref result 1 0 1) (formula 31 0 100)
+            (aref result 1 0 2) (formula 31 0 110)
+            (aref result 1 1 0) (formula 31 252 90)
+            (aref result 1 1 1) (formula 31 252 100)
+            (aref result 1 1 2) (formula 31 252 110))
+      result)))
+
+(defmethod handle-command ((cmd (eql :statranges)) (con connection) (msg message))
+  (let ((cmd (split-at-first #\ (cdr (parse-nickname-and-message msg)))))
+    (multiple-value-bind (lvl basestats) (match-level-and-base-stat (cdr cmd))
+      (when (and lvl basestats)
+        (reply con msg (handle-ev-iv-chart (other-formula-intervals lvl basestats)))))))
+
+
+(defmethod handle-command ((cmd (eql :scripts)) (con connection) (msg message))
+  (reply con msg "<a href=\"http://pokemon-online.eu/scripts.js\">PO Beta server Scripts.js</a>"))
+(defun handle-ev-iv-chart (array)
+  (cl-who:with-html-output-to-string (*standard-output*)
+    (:table :align :center :cellpadding 2 :cellspacing 0 :style"border-width:1px; border-style:solid; border-color:#000;"
+            (:thead :style"font-weight:bold;"
+                    (:tr :style"background-color:#b0b0b0;"
+                         (:td :align :center "--")
+                         (:td :align :center "Negative")
+                         (:td :align :center "Neutral")
+                         (:td :align :center "Positive")))
+            (:tbody (:tr :style"background-color:#c2c2c2;"
+                         (:td :align :right :style"font-weight:bold;" "Min EVs")
+                         (:td :align :center (str (aref array 1 0 0)))
+                         (:td :align :center (str (aref array 1 0 1)))
+                         (:td :align :center (str (aref array 1 0 2))))
+                    (:tr :style"background-color:#c2c2c2;"
+                         (:td :align :right :style"font-weight:bold;" "Max EVs")
+                         (:td :align :center (str (aref array 1 1 0)))
+                         (:td :align :center (str (aref array 1 1 1)))
+                         (:td :align :center (str (aref array 1 1 2))))))
+    
+    #+ () (:table :align :center :cellpadding 2 :cellspacing 0 :style"border-width:1px; border-style:solid; border-color:#000;"
+            (:thead :style"font-weight:bold;"
+                    (:tr :style"background-color:#b0b0b0;"
+                         (:td :rowspan 2 :align :center :style"vertical-align:middle;" (:img :src "Themes/Classic/client/uAvailable.png"))
+                         (:td :colspan 2 :align :center "Negative Nature")
+                         (:td :colspan 2 :align :center "Neutral Nature")
+                         (:td :colspan 2 :align :center "Positive Nature"))
+                    (:tr :style"background-color:#c0c0c0;"
+                         (:td :align :center "Min EVs")
+                         (:td :align :center "Max EVs")
+                         (:td :align :center "Min EVs")
+                         (:td :align :center "Max EVs")
+                         (:td :align :center "Min EVs")
+                         (:td :align :center "Max EVs")))
+            (:tbody (:tr :style"background-color:#c2c2c2;"
+                         (:td :align :right :style"font-weight:bold;" "Min IVs")
+                         (:td :align :center (str (aref array 0 0 0)))
+                         (:td :align :center (str (aref array 0 1 0)))
+                         (:td :align :center (str (aref array 0 0 1)))
+                         (:td :align :center (str (aref array 0 1 1)))
+                         (:td :align :center (str (aref array 0 0 2)))
+                         (:td :align :center (str (aref array 0 1 2))))
+                    (:tr :style"background-color:#c2c2c2;"
+                         (:td :align :right :style"font-weight:bold;" "Max IVs")
+                         (:td :align :center (str (aref array 1 0 0)))
+                         (:td :align :center (str (aref array 1 1 0)))
+                         (:td :align :center (str (aref array 1 0 1)))
+                         (:td :align :center (str (aref array 1 1 1)))
+                         (:td :align :center (str (aref array 1 0 2)))
+                         (:td :align :center (str (aref array 1 1 2))))))))
